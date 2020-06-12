a239174889f9535597f903c22b39281eb596f3e2
liuWuWeu <1362800761@qq.com>
2020-06-12 23:40:29 +0800

    sed and awk

---
+++ shell_3/sed.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cat d2.txt | sed -r '/400/800/'
+
+cat d2.txt | sed -r 's/(400)/(&)/'
+
+cat d2.txt | sed -r 's/([0-9]{3})([0-9]{3})/\1-\2-/'
+
+cat d2.txt | sed -r 's/.*\n.*\n.*\n//'
+pause

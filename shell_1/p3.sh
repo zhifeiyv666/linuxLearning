1c6f30164c8b222b407ad1355156fbd6e32ad646
root <root@localhost.localdomain>
2020-06-12 03:31:03 -0400

    loop statement

---
+++ shell_1/p3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+func_print(){
+	for((i=1;i<=9;i++))
+	do
+		for((j=1;j<=$i;j++))
+		do echo -n -e "\t$i * $j = $[ i*j ]"
+		done
+		echo ""
+	done
+}
+
+if [ $# > 1 ]
+then func_print >> $1
+else func_print
+fi
